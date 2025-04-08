91d8923131cc8d978c1dcc0a067f7467b7a65b9f
NathFELT <nathanfeltessepro@gmail.com>
2025-04-08 16:18:19 +0200

    AJOUT Classes models

--- src/modelPackage/Club.java
+++ src/modelPackage/Club.java
@@ -1,7 +1,30 @@
 package modelPackage;
 
+import java.time.LocalDate;
+
 public class Club {
 
-    private int clubID
+    private int clubID;
+    private String name;
+    private String streetAddress;
+    private int localityID;
+    private int phoneNumber;
+    private LocalDate creationDate;
+    private String website;
+    private boolean isBeginnersFriendly;
+    private String instagramProfile;
+
+    public Club(int clubID, String name, String streetAddress, int localityID, int phoneNumber, LocalDate creationDate, String website, boolean isBeginnersFriendly, String instagramProfile) {
+        this.clubID = clubID;
+        this.name = name;
+        this.streetAddress = streetAddress;
+        this.localityID = localityID;
+        this.phoneNumber = phoneNumber;
+        this.creationDate = creationDate;
+        this.website = website;
+        this.isBeginnersFriendly = isBeginnersFriendly;
+        this.instagramProfile = instagramProfile;
+    }
+
 
 }
