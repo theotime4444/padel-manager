dec675f5269c2ad2593757a6feb5205daa16dfc7
NathFELT <nathanfeltessepro@gmail.com>
2025-04-10 04:20:07 +0200

    AJOUT Classes models

--- src/modelPackage/Club.java
+++ src/modelPackage/Club.java
@@ -4,27 +4,46 @@ import java.time.LocalDate;
 
 public class Club {
 
-    private int clubID;
+    private Integer id;
+    private Integer localityID;
     private String name;
     private String streetAddress;
-    private int localityID;
     private int phoneNumber;
     private LocalDate creationDate;
     private String website;
     private boolean isBeginnersFriendly;
     private String instagramProfile;
 
-    public Club(int clubID, String name, String streetAddress, int localityID, int phoneNumber, LocalDate creationDate, String website, boolean isBeginnersFriendly, String instagramProfile) {
-        this.clubID = clubID;
-        this.name = name;
-        this.streetAddress = streetAddress;
-        this.localityID = localityID;
-        this.phoneNumber = phoneNumber;
-        this.creationDate = creationDate;
-        this.website = website;
-        this.isBeginnersFriendly = isBeginnersFriendly;
-        this.instagramProfile = instagramProfile;
+    public Club(int id, String name, String streetAddress, int localityID, int phoneNumber, LocalDate creationDate, String website, boolean isBeginnersFriendly, String instagramProfile) {
+
     }
+
+    public Integer getId() { return id; }
+    public void setId(Integer id) {this.id = id;}
+
+    public Integer getLocalityID() {return localityID;}
+    public void setLocalityID(Integer localityID) {this.localityID = localityID;}
+
+    public String getName() {return name;}
+    public void setName(String name) {this.name = name;}
+
+    public String getStreetAddress() {return streetAddress;}
+    public void setStreetAddress(String streetAddress) {this.streetAddress = streetAddress;}
+
+    public int getPhoneNumber() {return phoneNumber;}
+    public void setPhoneNumber(int phoneNumber) {this.phoneNumber = phoneNumber;}
+
+    public LocalDate getCreationDate() {return creationDate;}
+    public void setCreationDate(LocalDate creationDate) {this.creationDate = creationDate;}
+
+    public String getWebsite() {return website;}
+    public void setWebsite(String website) {this.website = website;}
+
+    public boolean isBeginnersFriendly() {return isBeginnersFriendly;}
+    public void setBeginnersFriendly(boolean beginnersFriendly) {isBeginnersFriendly = beginnersFriendly;}
+
+    public String getInstagramProfile() {return instagramProfile;}
+    public void setInstagramProfile(String instagramProfile) {this.instagramProfile = instagramProfile;}
 
 
 }
--- src/modelPackage/Participation.java
+++ src/modelPackage/Participation.java
@@ -1,4 +1,20 @@
 package modelPackage;
 
 public class Participation {
-}
+
+    private Integer playerId;
+    private Integer gameId;
+    private int score;
+
+    public Participation( int score) {this.score = score;}
+
+    public Integer getPlayerId() {return playerId;}
+    public void setPlayerId(Integer playerId) {this.playerId = playerId;}
+
+    public Integer getGameId() {return gameId;}
+    public void setGameId(Integer gameId) {this.gameId = gameId;}
+
+    public int getScore() {return score;}
+    public void setScore(int score) {this.score = score;}
+
+
