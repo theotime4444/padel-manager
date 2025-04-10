9e9ac7cdca59d0e741ef40cc9eaf5a26e15db4b8
NathFELT <nathanfeltessepro@gmail.com>
2025-04-10 16:58:02 +0200

    AJOUT Classes models

--- src/modelPackage/Participation.java
+++ src/modelPackage/Participation.java
@@ -6,15 +6,32 @@ public class Participation {
     private Integer gameId;
     private int score;
 
-    public Participation( int score) {this.score = score;}
+    public Participation(int score) {
+        this.score = score;
+    }
 
-    public Integer getPlayerId() {return playerId;}
-    public void setPlayerId(Integer playerId) {this.playerId = playerId;}
+    public Integer getPlayerId() {
+        return playerId;
+    }
 
-    public Integer getGameId() {return gameId;}
-    public void setGameId(Integer gameId) {this.gameId = gameId;}
+    public void setPlayerId(Integer playerId) {
+        this.playerId = playerId;
+    }
 
-    public int getScore() {return score;}
-    public void setScore(int score) {this.score = score;}
+    public Integer getGameId() {
+        return gameId;
+    }
 
+    public void setGameId(Integer gameId) {
+        this.gameId = gameId;
+    }
 
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+}
