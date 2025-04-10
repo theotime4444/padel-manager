e15f7689c513ae73e61a9c09db2903824c362859
theotime4444 <theotime4444@gmail.com>
2025-04-10 10:37:09 +0200

    Suppression du fichier PadelManager.iml et ajout des classes Court, Membership et Tournement avec leurs attributs et méthodes correspondants.

--- src/modelPackage/Court.java
+++ src/modelPackage/Court.java
@@ -1,4 +1,40 @@
 package modelPackage;
 
 public class Court {
+    private int id;
+    private String state;
+    private boolean isOutdoor;
+    private Club club;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public boolean isOutdoor() {
+        return isOutdoor;
+    }
+
+    public void setOutdoor(boolean outdoor) {
+        isOutdoor = outdoor;
+    }
+
+    public Club getClub() {
+        return club;
+    }
+
+    public void setClub(Club club) {
+        this.club = club;
+    }
 }
--- src/modelPackage/Membership.java
+++ src/modelPackage/Membership.java
@@ -1,4 +1,33 @@
 package modelPackage;
 
+import java.time.LocalDate;
+
 public class Membership {
+    private Club club;
+    private Player player;
+    private LocalDate registrationDate;
+
+    public Club getClub() {
+        return club;
+    }
+
+    public void setClub(Club club) {
+        this.club = club;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    public LocalDate getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(LocalDate registrationDate) {
+        this.registrationDate = registrationDate;
+    }
 }
